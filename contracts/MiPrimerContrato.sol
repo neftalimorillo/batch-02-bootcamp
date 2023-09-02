1dd8102186249978db48d67826a102a9d3492b6b
neftalimorillo <morilloneftali@gmail.com>
2023-09-02 10:38:45 -0500

    Update MiPrimerContrato.sol

--- contracts/MiPrimerContrato.sol
+++ contracts/MiPrimerContrato.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 contract MiPrimerContrato {
     string saludo;
-    string nombreYApellido = "Lee Marreros";
+    string NeftaliMorillo = "Lee Marreros";
 
     function set(string memory _nuevoSaludo) public {
         saludo = _nuevoSaludo; // no se necesita 'this'
